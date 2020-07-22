e4bc8b9a12dd8e90117648de81f5e89fd3f00a4e
cdmcs <540928028@qq.com>
2020-07-22 16:26:59 +0800

    初始化项目

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>一个react项目</p>
+            </div>
+        );
+    }
+}
